8ac8106ef62974528a4bb103f2e252335d78062f
Bushra Khan <bushrakhan2212002@gmail.com>
2023-01-29 23:34:11 +0530

    line image

--- Chapter-4/shapes-image.py
+++ Chapter-4/shapes-image.py
@@ -6,7 +6,7 @@ print(img.shape)
 cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),2)
 cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED)
 cv2.circle(img,(400,50),50, (255,255,0))
-
+cv2.putText(img,"OPEN CV TEXT",(200,200),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,150,0),2)
 cv2.imshow("Image",img)
 
 cv2.waitKey(0)
