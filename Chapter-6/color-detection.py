8100231c7624d8fc60ab7db0e32fc84012ce08e0
Bushra Khan <bushrakhan2212002@gmail.com>
2023-01-30 23:52:48 +0530

    step 1

--- Chapter-6/color-detection.py
+++ Chapter-6/color-detection.py
@@ -2,10 +2,10 @@ import cv2
 
 img = cv2.imread("../Resources/car1.png")
 cv2.namedWindow("Trackbars")
-cv2.resize("Trackbars",640,240)
+cv2.resizeWindow("Trackbars",640,240)
 def empty(a):
     pass
-cv2.createTrackbar("Hue Min","Trackbar",0,179,empty)
+cv2.createTrackbar("Hue Min","Trackbars",0,179,empty)
 imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 img1 = cv2.resize(img,(400,300))
