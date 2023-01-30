2519b9449fbf2a75ac1d4c60d18137939a79a525
Bushra Khan <bushrakhan2212002@gmail.com>
2023-01-30 23:44:25 +0530

    step 1

---
+++ Chapter-6/color-detection.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread("../Resources/car1.png")
+imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+img1 = cv2.resize(img,(400,300))
+imgHSV1 = cv2.resize(imgHSV,(400,300))
+
+cv2.imshow("Original",img1)
+
+cv2.imshow("HSV",imgHSV1)
+
+cv2.waitKey(0)
