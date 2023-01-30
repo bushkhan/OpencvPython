05346fe261ace7457a383bca7a5b1f572862e0e0
Bushra Khan <bushrakhan2212002@gmail.com>
2023-01-30 23:19:27 +0530

    join images

--- Chapter-5/joining-images.py
+++ Chapter-5/joining-images.py
@@ -5,8 +5,11 @@ import numpy as np
 img = cv2.imread('../Resources/girl.png')
 imgResize = cv2.resize(img,(600,400))
 
-imHor = np.hstack((imgResize,imgResize))
+imHor = np.hstack((imgResize,imgResize)) #horizontal satcking
+imVer = np.vstack((imgResize,imgResize)) #Vertical satcking
+
 
 cv2.imshow("Horizontal", imHor)
+cv2.imshow("Vertical", imVer)
 
 cv2.waitKey(0)
